eb9af559d4e43c7f0a26f306aedfe2152a7b89b3
greatSumini <greatSumini@gmail.com>
2021-11-27 18:26:32 +0900

    feat(): add og tags
    - title, type, url, description, image

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,11 @@ export default function Home() {
       <Head>
         <title>AWESOME.DEV</title>
         <meta name="description" content="가장 AWESOME한 개발 파트너" />
+        <meta property="og:title" content="AWESOME.DEV" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://awesome.dev" />
+        <meta property="og:description" content="가장 AWESOME한 개발 파트너" />
+        <meta property="og:image" content="/images/OG.png" />
         <link rel="icon" href="/images/favicon.png" />
       </Head>
 
