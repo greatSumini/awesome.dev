60092444d2deac5ee40761eddf72f24cb62ef18a
greatSumini <greatSumini@gmail.com>
2024-07-03 04:50:54 +0900

    feat: add description

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,11 +7,14 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>AWESOME.DEV</title>
-        <meta name="description" content="가장 AWESOME한 개발 파트너" />
+        <meta name="description" content="CRM · 그룹웨어 전문 외주개발사" />
         <meta property="og:title" content="AWESOME.DEV" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://awesome.dev" />
-        <meta property="og:description" content="가장 AWESOME한 개발 파트너" />
+        <meta
+          property="og:description"
+          content="CRM · 그룹웨어 전문 외주개발사"
+        />
         <meta property="og:image" content="/images/OG.png" />
         <link rel="icon" href="/images/favicon.png" />
       </Head>
@@ -22,11 +25,21 @@ export default function Home() {
           <br />
           DEV
         </h1>
+
+        <p className={styles.description}>CRM · 그룹웨어 전문 외주개발사</p>
+        <br />
+
+        <ul style={{ textAlign: "center", listStyleType: "none", padding: 0 }}>
+          <li>Big-Tech 출신 인력이 직접 개발</li>
+          <li>개인정보/보안 완벽 대응</li>
+          <li>실시간 작업 공유/배포</li>
+          <li>무제한, 무료 하자보수 100% 보증</li>
+        </ul>
       </main>
 
       <footer className={styles.footer}>
         contact:
-        <a className={styles.code}>+82 010-4413-1261</a>
+        <a className={styles.code}>+82 10 4413 1261</a>
         <a href="mailto:lead@awesome.dev" className={styles.code}>
           lead@awesome.dev
         </a>
